9c7a4d0913994f844371209dfa008eb45fa1c651
Arnaud Sene <arnaud.sene@pm.me>
2023-08-05 00:12:40 -0400

    init: Minimal Next.js typescript tailwind CSS Template
    - NEXT.js project
    - typescript
    - tailwind CSS
    - Responsive Nav, section, footer template

---
+++ constants/index.ts
@@ -0,0 +1,72 @@
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "feat1",
+        title: "Feat1",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+]
+
+export const footerLinks = [
+    {
+        title: "Useful Links",
+        links: [
+        {
+            name: "Content",
+            link: "a link",
+        },
+        {
+            name: "How it Works",
+            link: "a link",
+        },
+        {
+            name: "Terms & Services",
+            link: "a link",
+        },
+        ]
+    },
+    {
+        title: "Community",
+        links: [
+          {
+            name: "Help Center",
+            link: "a link",
+          },
+          {
+            name: "Partners",
+            link: "a link",
+          },
+          {
+            name: "Suggestions",
+            link: "a link",
+          },
+          {
+            name: "Blog",
+            link: "a link",
+          },
+          {
+            name: "Newsletters",
+            link: "a link",
+          },
+        ],
+      },
+      {
+        title: "Partner",
+        links: [
+          {
+            name: "Our Partner",
+            link: "a link",
+          },
+          {
+            name: "Become a Partner",
+            link: "a link",
+          },
+        ],
+      },
+]
